e1ebc64840c14af214fce6b47c59de5233b42ddd
Gabriel-Alexandru Badescu <badescuga@gmail.com>
2015-08-26 20:14:08 +0300

    enabled ES7 async methods..

--- js/handler.js
+++ js/handler.js
@@ -1,3 +1,13 @@
 //var db = require('./db.js');
 import * as  db from './db.js'
-console.log(` test:  ${ db.tt } ` +db.tt);
+console.log(` test:  ${ db.tt } ` + db.tt);
+
+
+async function loginUser(data, callback) {
+
+};
+
+
+module.exports = {
+	loginUser: loginUser
+};
--- server.js
+++ server.js
@@ -1,2 +1,6 @@
-require("babel/register");
+require('babel/register')({
+  optional: ["es7.asyncFunctions"]
+});
+
+
 var app = require("./app.js");
