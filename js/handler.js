ec2f178a8282e5596e7c84f1412e77a016704671
Alex Badescu <badescuga@gmail.com>
2015-08-30 18:09:23 +0300

    starting investigating async impl for azure-storage

--- js/handler.js
+++ js/handler.js
@@ -1,3 +1,5 @@
 //var db = require('./db.js');
-import * as  db from './db.js'
+import * as  db from './db'
+import * as  testAsync from './testStorageAsync'
+
 console.log(` test:  ${ db.tt } ` +db.tt);
